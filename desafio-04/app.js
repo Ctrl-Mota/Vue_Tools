44731e6262e76ab955cfdeaff704d71e171209dd
Lucas Mota <lucas.d.mota@gmail.com>
2020-11-03 16:23:48 -0300

    desafio4

--- desafio-04/app.js
+++ desafio-04/app.js
@@ -1,14 +1,33 @@
 new Vue({
 	el: '#desafio',
+	efeito: '#efeito',
 	data: {
+		efects: true,
+		classEfeito: '',
+		classesCss: ['cor','tamanhoTxt'],
+		width: '0',
 
 	},
 	methods: {
 		iniciarEfeito() {
-
+			setInterval(()=>{
+				
+				if(this.efects){
+					this.classEfeito = 'destaque'
+				}else{
+					this.classEfeito = "encolher"
+				}
+				this.efects = !this.efects
+				
+			}, 2000)
 		},
 		iniciarProgresso() {
-
+			let valor = 0
+			const temp = setInterval(()=>{
+				valor += 0.6
+				this.width = `${valor}%`;
+				if(valor >= 100) clearInterval(temp)
+			}, 15)
 		}
 	}
 })
